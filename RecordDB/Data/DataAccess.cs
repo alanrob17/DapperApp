1ac40538c1723bc77cc126825e3648f9675ec880
Alan Robson <alanr@live.com.au>
2025-05-25 17:44:03 +1000

    Minor generic cost method changes.

--- RecordDB/Data/DataAccess.cs
+++ RecordDB/Data/DataAccess.cs
@@ -152,10 +152,7 @@ namespace RecordDB.Data
 
             try
             {
-                var cost = await connection.ExecuteScalarAsync<decimal>(
-                    storedProcedureName,
-                    parameters,
-                    commandType: CommandType.StoredProcedure);
+                var cost = await connection.ExecuteScalarAsync<decimal>(storedProcedureName, parameters, commandType: CommandType.StoredProcedure);
 
                 _logger.LogInformation("Successfully executed stored procedure {Procedure}, returned value: {Value}", storedProcedureName, cost);
                 return cost;
@@ -183,48 +180,5 @@ namespace RecordDB.Data
                 throw;
             }
         }
-
-        //public async Task<decimal> GetCostAsync<T>(string storedProcedureName)
-        //{
-        //    return await GetCostAsync<T>(storedProcedureName, null);
-        //}
-
-        //public async Task<decimal> GetCostAsync<T>(string storedProcedureName, object parameter = null)
-        //{
-        //    using var connection = _connectionFactory.CreateConnection();
-        //    decimal cost = 0.0m;
-
-        //    try
-        //    {
-        //        cost = await connection.ExecuteScalarAsync<decimal>(storedProcedureName, parameter, commandType: CommandType.StoredProcedure);
-
-        //        _logger.LogInformation("Successfully executed cost query: {Procedure}, returned cost: {Cost}", storedProcedureName, cost);
-        //        return cost;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        _logger.LogError(ex, "Error executing Cost query using parameter: {parameter} and procedure {Procedure}", parameter, storedProcedureName);
-        //        throw;
-        //    }
-        //}
-
-        //public async Task<decimal> GetCostQueryAsync<T>(string query)
-        //{
-        //    using var connection = _connectionFactory.CreateConnection();
-        //    decimal cost = 0.0m;
-
-        //    try
-        //    {
-        //       cost = await connection.ExecuteScalarAsync<decimal>(query);
-
-        //        _logger.LogInformation("Successfully executed cost query: {Query}, returned cost: {Cost}", query, cost);
-        //        return cost;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        _logger.LogError(ex, "Error executing cost query: {Query}", query);
-        //        throw;
-        //    }
-        //}
     }
 }
